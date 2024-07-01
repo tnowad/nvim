8180af2a4c9444e36059c5dfec581379be41addc
Nguyen Minh Tuan <tnowad@gmail.com>
2024-07-02 06:54:33 +0700

    feat: add config keymaps for neogit

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -6,5 +6,20 @@ return {
 
     "nvim-telescope/telescope.nvim",
   },
-  config = true,
+  config = function()
+    require("neogit").setup({
+      disable_signs = false,
+      disable_context_highlighting = false,
+      disable_commit_confirmation = false,
+      disable_builtin_notifications = false,
+      commit_popup = {
+        kind = "split",
+      },
+      integrations = {
+        diffview = true,
+      },
+    })
+
+    vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>Neogit<CR>", { noremap = true, silent = true })
+  end,
 }
