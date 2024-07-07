fdf5ebecc5b92af7e715bf9e7e3c84544c01fe24
Nguyen Minh Tuan <tnowad@gmail.com>
2024-07-08 01:43:52 +0700

    feat: add copilot chat plugin

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -32,4 +32,15 @@ return {
       })
     end,
   },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+    },
+    opts = {
+      debug = false,
+    },
+  },
 }
