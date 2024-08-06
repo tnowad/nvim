941488c8e73f5649be0116a7429ae3af2ef44b09
Nguyen Minh Tuan <tnowad@gmail.com>
2024-08-06 14:03:38 +0700

    fix: remove nested object syntax in conform

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -11,12 +11,11 @@ return {
       desc = "[F]ormat buffer",
     },
   },
+
+  ---@type conform.setupOpts
   opts = {
     notify_on_error = false,
     format_on_save = function(bufnr)
-      -- Disable "format_on_save lsp_fallback" for languages that don't
-      -- have a well standardized coding style. You can add additional
-      -- languages here or re-enable it for the disabled ones.
       local disable_filetypes = { c = true, cpp = true }
       return {
         timeout_ms = 500,
@@ -25,15 +24,10 @@ return {
     end,
     formatters_by_ft = {
       lua = { "stylua" },
-      -- Conform can also run multiple formatters sequentially
-      -- python = { "isort", "black" },
-      --
-      -- You can use a sub-list to tell conform to run *until* a formatter
-      -- is found.
-      javascript = { { "prettierd", "prettier" } },
-      typescript = { { "prettierd", "prettier" } },
-      typescriptreact = { { "prettierd", "prettier" } },
-      javascriptreact = { { "prettierd", "prettier" } },
+      javascript = { "prettierd" },
+      typescript = { "prettierd" },
+      typescriptreact = { "prettierd" },
+      javascriptreact = { "prettierd" },
       json = { "prettierd" },
       yaml = { "prettierd" },
       markdown = { "prettierd" },
