a99fc6f7be2d080763f02c3565b56901b568d1da
Nguyen Minh Tuan <tnowad@gmail.com>
2025-02-03 02:08:45 +0700

    chore: remove harpoon and update which-key

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,32 +1,12 @@
+---@type LazyPluginSpec
 return {
   "folke/which-key.nvim",
-  event = "VimEnter",
-  config = function()
-    require("which-key").setup()
-
-    -- Document existing key chains
-    require("which-key").add({
-      { "<leader>c", group = "[C]ode" },
-      { "<leader>c_", hidden = true },
-      { "<leader>d", group = "[D]ocument" },
-      { "<leader>d_", hidden = true },
-      { "<leader>h", group = "Git [H]unk" },
-      { "<leader>h_", hidden = true },
-      { "<leader>r", group = "[R]ename" },
-      { "<leader>r_", hidden = true },
-      { "<leader>s", group = "[S]earch" },
-      { "<leader>s_", hidden = true },
-      { "<leader>t", group = "[T]oggle" },
-      { "<leader>t_", hidden = true },
-      { "<leader>w", group = "[W]orkspace" },
-      { "<leader>w_", hidden = true },
-    })
-    -- visual mode
-    require("which-key").add({
-      {
-        "<leader>h",
-        group = "Git [H]unk",
-      },
-    }, { mode = "v" })
-  end,
+  event = "VeryLazy",
+  ---@type wk.Opts
+  ---@diagnostic disable-next-line: missing-fields
+  opts = {
+    icons = {
+      mappings = false,
+    },
+  },
 }
