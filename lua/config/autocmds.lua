49a300f7df752b900264a105e256e93ab9864198
Nguyen Minh Tuan <tnowad@gmail.com>
2025-03-24 18:29:11 +0700

    feat: add auto command to create directory before writing file
    - Added an auto command that prompts the user to create a directory if it doesn't exist when writing a file.
    - If the directory doesn't exist, the user is asked if they want to create it. If they choose "y", the directory is created. If they choose "n", the write operation is aborted.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -5,3 +5,31 @@ vim.api.nvim_create_autocmd("TextYankPost", {
     vim.highlight.on_yank()
   end,
 })
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  desc = "Create directory before writing file",
+  pattern = "*",
+  group = vim.api.nvim_create_augroup("create-dir", { clear = true }),
+  callback = function()
+    local filepath = vim.fn.expand("<afile>:p")
+    local dir = vim.fn.fnamemodify(filepath, ":h")
+
+    if dir == "" or vim.fn.isdirectory(dir) == 1 then
+      return
+    end
+
+    local choice = vim.fn.input("Directory '" .. dir .. "' does not exist. Create? (y/n): ")
+    if choice:lower() == "y" then
+      if vim.fn.mkdir(dir, "p") == 1 then
+        print("\nCreated directory: " .. dir)
+      else
+        print("\nError: Failed to create directory: " .. dir)
+        return
+      end
+    else
+      print("\nAborted: Directory not created.")
+      vim.cmd("echohl ErrorMsg | echom 'Write aborted' | echohl None")
+      vim.cmd("silent! undo")
+    end
+  end,
+})
