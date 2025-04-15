6875e00a6d9da2cb9b090865c6863be479e0887e
Nguyen Minh Tuan <tnowad@gmail.com>
2025-04-16 02:28:56 +0700

    feat: change new cmp to blink

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,36 +1,96 @@
 return {
-  "hrsh7th/nvim-cmp",
-  event = "InsertEnter",
+  "saghen/blink.cmp",
   dependencies = {
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-path",
+    "mikavilpas/blink-ripgrep.nvim",
   },
+  version = "1.*",
 
-  config = function()
-    local cmp = require("cmp")
-    cmp.setup({
-      completion = {
-        completeopt = "menu,menuone,noinsert",
-      },
-      mapping = cmp.mapping.preset.insert({
-        ["<C-n>"] = cmp.mapping.select_next_item(),
-        ["<C-p>"] = cmp.mapping.select_prev_item(),
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-        ["<C-Space>"] = cmp.mapping.complete({}),
-      }),
-      sources = {
-        { name = "nvim_lsp" },
-        { name = "path" },
-      },
-    })
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    keymap = { preset = "default" },
 
-    cmp.setup.filetype("copilot-chat", {
-      sources = {
-        { name = "buffer" },
-        { name = "path" },
+    signature = { enabled = true },
+
+    completion = {
+      documentation = { auto_show = true },
+      menu = {
+        draw = {
+          columns = {
+            { "label", "label_description" },
+            { "kind_icon" },
+          },
+        },
       },
-    })
-  end,
+    },
+
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer", "ripgrep" },
+      providers = {
+        ripgrep = {
+          module = "blink-ripgrep",
+          name = "Ripgrep",
+          ---@module "blink-ripgrep"
+          ---@type blink-ripgrep.Options
+          opts = {
+            prefix_min_len = 3,
+            context_size = 5,
+            max_filesize = "1M",
+            project_root_marker = ".git",
+            project_root_fallback = true,
+            search_casing = "--ignore-case",
+            fallback_to_regex_highlighting = false,
+            future_features = {
+              backend = {
+                use = "gitgrep-or-ripgrep",
+              },
+            },
+          },
+          transform_items = function(_, items)
+            for _, item in ipairs(items) do
+              item.labelDetails = {
+                description = "(rg)",
+              }
+            end
+            return items
+          end,
+        },
+      },
+    },
+
+    appearance = {
+      kind_icons = {
+        Text = "Text",
+        Method = "Meth",
+        Function = "Func",
+        Constructor = "Cons",
+
+        Field = "Field",
+        Variable = "Var",
+        Property = "Prop",
+
+        Class = "Clas",
+        Interface = "Inte",
+        Struct = "Stru",
+        Module = "Modu",
+
+        Unit = "Unit",
+        Value = "Valu",
+        Enum = "Enum",
+        EnumMember = "Emem",
+
+        Keyword = "Keyw",
+        Constant = "Cons",
+
+        Snippet = "Snip",
+        Color = "Colr",
+        File = "File",
+        Reference = "Ref",
+        Folder = "Fold",
+        Event = "Even",
+        Operator = "Oper",
+        TypeParameter = "Type",
+      },
+    },
+  },
 }
