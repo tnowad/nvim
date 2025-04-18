fb7cdc33fb2cb08cf0fcae4d0c6bf528a6082262
Nguyen Minh Tuan <tnowad@gmail.com>
2025-04-18 19:46:39 +0700

    feat: add tailwindcss opts

--- lua/plugins/extras/lang/tailwindcss.lua
+++ lua/plugins/extras/lang/tailwindcss.lua
@@ -5,7 +5,7 @@ return {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     opts = utils.merge_with_unique_lists({
       ensure_installed = {
-        "tailwindcss-language-server",
+        "tailwindcss",
       },
     }),
   },
@@ -15,10 +15,106 @@ return {
     opts = utils.merge_with_unique_lists({
       servers = {
         tailwindcss = {
-          init_options = {
-            userLanguages = {
-              typescriptreact = "javascript",
-              javascriptreact = "javascript",
+          filetypes = {
+            'aspnetcorerazor',
+            'astro',
+            'astro-markdown',
+            'blade',
+            'clojure',
+            'django-html',
+            'htmldjango',
+            'edge',
+            'eelixir', -- vim ft
+            'elixir',
+            'ejs',
+            'erb',
+            'eruby', -- vim ft
+            'gohtml',
+            'gohtmltmpl',
+            'haml',
+            'handlebars',
+            'hbs',
+            'html',
+            'htmlangular',
+            'html-eex',
+            'heex',
+            'jade',
+            'leaf',
+            'liquid',
+            'markdown',
+            'mdx',
+            'mustache',
+            'njk',
+            'nunjucks',
+            'php',
+            'razor',
+            'slim',
+            'twig',
+            -- css
+            'css',
+            'less',
+            'postcss',
+            'sass',
+            'scss',
+            'stylus',
+            'sugarss',
+            -- js
+            'javascript',
+            'javascriptreact',
+            'reason',
+            'rescript',
+            'typescript',
+            'typescriptreact',
+            -- mixed
+            'vue',
+            'svelte',
+            'templ',
+          },
+          root_dir = require('lspconfig').util.root_pattern(
+            'tailwind.config.js',
+            'tailwind.config.cjs',
+            'tailwind.config.mjs',
+            'tailwind.config.ts',
+            'postcss.config.js',
+            'postcss.config.cjs',
+            'postcss.config.mjs',
+            'postcss.config.ts'
+          ),
+          settings = {
+            tailwindCSS = {
+              experimental = {
+                classRegex = {
+                  'cn\\(([^)]*)\\)',
+                  'clsx\\(([^)]*)\\)',
+                  'cva\\(([^)]*)\\)',
+                  'twMerge\\(([^)]*)\\)',
+                }
+              },
+              validate = true,
+              lint = {
+                cssConflict = 'warning',
+                invalidApply = 'error',
+                invalidConfigPath = 'error',
+                invalidScreen = 'error',
+                invalidTailwindDirective = 'error',
+                invalidVariant = 'error',
+                recommendedVariantOrder = 'warning',
+                unusedClasses = 'warning',
+              },
+              includeLanguages = {
+                css = 'css',
+                javascript = 'javascript',
+                javascriptreact = 'javascript',
+                typescript = 'javascript',
+                typescriptreact = 'javascript',
+                svelte = 'html',
+                vue = 'html',
+                html = 'html',
+                php = 'html',
+                eelixir = 'html-eex',
+                eruby = 'erb',
+                rust = 'html',
+              },
             },
           },
         },
