35955a2d4710e7d3c2bcd11ebdfb7a3da81611c1
Nguyen Minh Tuan <tnowad@gmail.com>
2025-04-23 23:47:47 +0700

    feat: add mini.bracketed plugin

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -15,5 +15,24 @@ return {
       },
     },
   },
+  {
+    "echasnovski/mini.bracketed",
+    opts = {
+      buffer     = { suffix = 'b', options = {} },
+      comment    = { suffix = 'c', options = {} },
+      conflict   = { suffix = 'x', options = {} },
+      diagnostic = { suffix = 'd', options = {} },
+      file       = { suffix = 'f', options = {} },
+      indent     = { suffix = 'i', options = {} },
+      jump       = { suffix = 'j', options = {} },
+      location   = { suffix = 'l', options = {} },
+      oldfile    = { suffix = 'o', options = {} },
+      quickfix   = { suffix = 'q', options = {} },
+      treesitter = { suffix = 't', options = {} },
+      undo       = { suffix = 'u', options = {} },
+      window     = { suffix = 'w', options = {} },
+      yank       = { suffix = 'y', options = {} },
+    }
+  },
   { 'echasnovski/mini.comment' },
 }
